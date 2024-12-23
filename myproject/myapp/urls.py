f9b05bd5557e94250d279067b8fa4af95eefcbe1
unknown <bexruzraxmanov59@gmail.com>
2024-12-23 18:28:31 +0500

    end

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import UsetLisrView
+from .views import UsetLisrView,BookDetailView
 
 urlpatterns = [
     path('', UsetLisrView.as_view(), name='list_user'),
+    path('<int:book_id>/',BookDetailView.as_view(),name='book-detail')
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -20,27 +20,25 @@ class UsetLisrView(APIView):
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
             
 
-
-
-# class BookDetailView(APIView):
-#     def get_object(self,book_id):
-#         data = read_date()
-#         for book in data:
-#             if book['id'] == book_id:
-#                 return book
-#         return None    
+class BookDetailView(APIView):
+    def get_object(self,book_id):
+        date = read_date()
+        for book in date:
+            if book['id'] == book_id:
+                return book
+        return None    
     
-#     def get(self,request,book_id):
-#         book = self.get_object(book_id)
-#         if book is not None :
-#             return Response(book)
-#         return Response({"error","Oshibka s book_id"})   
+    def get(self,request,book_id):
+        book = self.get_object(book_id)
+        if book is not None :
+            return Response(book)
+        return Response({"error","Oshibka s book_id"})   
     
-#     def delete(self,request,book_id):
-#         data = read_date()
-#         book = self.get_object(book_id)
-#         if book is None:
-#             return Response(book) 
-#         data = [b for b in data if b['id'] != book_id]
-#         write_date(data)
-#         return Response({"message","Id is deleted sucessfule"},status=status.HTTP_204_NO_CONTENT)
+    def delete(self,request,book_id):
+        date = read_date()
+        book = self.get_object(book_id)
+        if book is None:
+            return Response(book) 
+        date = [b for b in date if b['id'] != book_id]
+        write_date(date)
+        return Response({"message","Id is deleted sucessfule"},status=status.HTTP_204_NO_CONTENT)
